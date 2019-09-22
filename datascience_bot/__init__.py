9ee2a00c91f2e08126fc600bed01da95d3546b55
vogt4nick <vogt4nick@gmail.com>
2019-09-22 08:10:18 -0400

    Bump version: 2019.9.22.dev0 → 2019.9.22.dev1

--- datascience_bot/__init__.py
+++ datascience_bot/__init__.py
@@ -2,7 +2,7 @@
 __author__ = "vogt4nick"
 __copyright__ = "Copyright 2019, Nick Vogt"
 __license__ = "MIT"
-__version__ = "2019.9.22.dev0"
+__version__ = "2019.9.22.dev1"
 __maintainer__ = "vogt4nick"
 __email__ = "vogt4nick@gmail.com"
 __status__ = "Production"
