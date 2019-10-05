d85685956c89ae12e4f2d796fef587448d463307
vogt4nick <vogt4nick@gmail.com>
2019-10-05 18:42:37 -0400

    include wiki/*.md files in MANIFEST.in

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         ]
     },
     install_requires=requirements,
+    include_package_data=True,  # include MANIFEST.in
     test_suite="tests",
     tests_require="pytest",
     classifiers=[
