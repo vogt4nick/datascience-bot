e41d02236475b4434f53d5e492d0cbc354944459
vogt4nick <vogt4nick@gmail.com>
2019-10-05 20:33:21 -0400

    fix tests and update_wiki behavior

--- datascience_bot/cli/update_wiki.py
+++ datascience_bot/cli/update_wiki.py
@@ -13,7 +13,6 @@ from datascience_bot import get_datascience_bot, add_boilerplate, __version__
 
 # config logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s UTC | %(levelname)s | %(message)s")
@@ -30,10 +29,9 @@ def main():
 
     local_dir = (pathlib.Path(__file__).parent / ".." / "wiki").resolve()
     for f in local_dir.iterdir():
-        if f.is_dir():
-            continue
         page_name = f.stem
-        logging.info(f"Updating wiki page: {page_name}")
+        if f.is_dir() or f.suffix != ".md":
+            continue
 
         new_content = f.read_text()
         wiki_page = subreddit.wiki[page_name]
--- tests/test_update_wiki.py
+++ tests/test_update_wiki.py
@@ -27,7 +27,7 @@ def test__update_wiki(datascience_bot_reddit):
         {
             path.stem: path.read_text()
             for path in local_wiki_dir.iterdir()
-            if not path.is_dir()
+            if not path.is_dir() and path.suffix == ".md"
         },
     )
 
@@ -40,5 +40,6 @@ def test__update_wiki(datascience_bot_reddit):
     update_wiki.main()
 
     for wiki_page in datascience_bot.subreddit(SUBREDDIT_NAME).wiki:
-        # wiki content doesn't keep newlines at end of file, though we write them
-        assert wiki_page.content_md == local_wiki_content[wiki_page.name].strip()
+        if wiki_page.name in local_wiki_content:
+            # wiki content doesn't keep newlines at end of file, though we write them
+            assert wiki_page.content_md == local_wiki_content[wiki_page.name].strip()
